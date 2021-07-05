e73e952b3ee13aae62e4317bb28fc4ba226fe871
안찬경 <42137152+chan-gyeong@users.noreply.github.com>
2021-07-05 09:26:08 +0900

    Update BackEndSource.c

--- BackEndSource.c
+++ BackEndSource.c
@@ -7,7 +7,7 @@
 
 int main(void){
 	
-	FILE *fp = fopen("hello.txt", "wt");     // hello.txt 파일을 쓰기 모드(w)로 열기./
+	FILE *fp = fopen("hello.txt", "wt");     // hello.txt 파일을 쓰기 모드(w)로 열기.
 	
 	for (int n=1; n<100000000; n++){
 		fprintf(fp, "%d\n", NstPrime2(n));
